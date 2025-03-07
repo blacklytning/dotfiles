396f2bb2291d828a70e97ee1ba94537105614339
blacklytning <blacklytning@yandex.com>
2025-03-07 20:54:39 +0530

    Add yazi wrapper for fish

--- config.fish
+++ config.fish
@@ -13,3 +13,12 @@ end
 # <<< conda initialize <<<
 
 set -g fish_greeting
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
